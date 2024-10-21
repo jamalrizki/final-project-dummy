66b330175554e0ec50118e91e11e36d93f3afc3b
jamal rizki <73854356+jamalrizki@users.noreply.github.com>
2024-10-20 22:20:03 -0600

    sun

--- backend/src/routes/api/articles.js
+++ backend/src/routes/api/articles.js
@@ -1,5 +1,17 @@
 import express from "express";
 import { getArticles, addArticles, getArticleById} from "../../data/articles-dao.js";
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/') 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)) 
+  }
+})
+
 
 const router = express.Router();
 
@@ -12,11 +24,29 @@ router.get("/", async (req, res) => {
   return res.cookie("callCount", callCount.toString(), { httpOnly: true }).json(articles);
 });
 
-router.post("/", async (req, res) => {
-  const { article } = req.body;
+const upload = multer({ storage: storage })
+
+router.post("/", upload.single('image'), async (req, res) => {
+  const { title, description, content } = req.body;
+  const image = req.file ? req.file.filename : null;
+
+  // need to pull the user data from user route will jsut hard code to start
+  const username = "system_user";
+  const userAvatar = "Dragonite.png";
+
+  const article = {
+    title,
+    description,
+    content,
+    image,
+    date: new Date().toISOString().split('T')[0],
+    username,
+    userAvatar
+  };
+
   const dbMessage = await addArticles(article);
-  return res.status(201).location(`/api/articles/${article.id}`).json(dbMessage);
-})
+  return res.status(201).location(`/api/articles/${dbMessage.id}`).json(dbMessage);
+});
 
 router.get("/:id", async (req, res) => {
     const articleId = req.params.id;
